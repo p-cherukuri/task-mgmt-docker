7d9123005ef2a9ecf46e438a141bbe71a81d4846
p-cherukuri <phani.cheruk@gmail.com>
2017-09-08 12:08:00 -0400

    initialized a new vue app in the ui folder

---
+++ ui/src/components/TaskList/TaskList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul>
+            <li> Task 1 </li>
+            <li> Task 2 </li>
+            <li> Task 3 </li>
+        </ul>
+    </div>
+</template>
+
+<script type = "text/javascript">
+export default {
+};
+</script>
+
+<style>
+
+</style>
+
